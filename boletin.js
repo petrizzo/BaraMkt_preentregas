5a01fd24c7e236e598a45922ad825960659b3ad3
petrizzo <petrizzo@gmail.com>
2023-06-07 23:00:50 -0400

    Actualización archivo boletinjs

--- boletin.js
+++ boletin.js
@@ -2,8 +2,10 @@ let informacion = { nombreApellido : prompt("Ingrese su nombre y su apellido"),
     eMail : prompt("Ingrese su email"), telefono : prompt("Ingrese su numero de telefono"),
 };
 let informacionConvertida = JSON.stringify(informacion);
+// let baseDatos = JSON.parse(informacionConvertida);
 
-console.log(informacionConvertida);
+// let datosArray = JSON.push(informacion);
+// let actualizarJSON = JSON.stringify(datosArray)
  
 // Declarar un array de objetos y almacenar el array en formato JSON en storage.
 
@@ -26,15 +28,25 @@ obj2: objetoDos,
 let arrayJSON = JSON.stringify(arrayObjetos);
 localStorage.setItem('arrayObjetos', arrayJSON);
 
+// alert("El usuario" + ' ' + nombreApellido + ' ' + "con el correo" + ' ' + eMail + ' ' + "y el teléfono" + ' ' + telefono + " ha sido agregado");
 
 
+//Base de datos
+
+// let arrayJson = {
+//     "Celeste":"Perez",
+//     "tzirw@example.com": "Blanca",
+//     "Lopez":"tzirw_1@example.comLopez",
+//     };
+//     let jsonConvertido = JSON.parse(arrayJson);
+//     alert(jsonConvertido)
 
 
 // almacenamos en localStorage
 
-localStorage.setItem('nombreApellido', 'maria');
-localStorage.setItem('eMail', 'maria@gmail.com');
-localStorage.setItem('telefono', '123456');
+localStorage.setItem('nombreApellido', 'nombreApellido');
+localStorage.setItem('eMail', 'eMail');
+localStorage.setItem('telefono', 'telefono');
 
 // accediendo a información del localstorage
 
@@ -42,9 +54,5 @@ let nombre = localStorage.getItem('nombreApellido');
 let correo = localStorage.getItem('eMail');
 let numero = localStorage.getItem('telefono');
 
-// mostrando el tipo de las variables
-
-console.log(typeof nombre);
-console.log(typeof correo);
-console.log(typeof numero);
+alert("El usuario" + ' ' + nombre + ' ' + "con el correo" + ' ' + correo + ' ' + "y el teléfono" + ' ' + numero + " ha sido agregado");
 
