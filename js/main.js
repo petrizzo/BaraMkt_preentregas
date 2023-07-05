fc4fffb1dee3064e9ed0dcf5c84cdf15768ce136
petrizzo <petrizzo@gmail.com>
2023-07-05 04:59:44 -0400

    Corrección de resultado de API en orden alfabético

--- js/main.js
+++ js/main.js
@@ -3,18 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 const selectDrop = document.querySelector('#paises');
 
 fetch('https://restcountries.com/v3.1/all').then(res => {
-
 	return res.json();
 }). then(data => {
-	let output = "";
+	let selection = "";
+	data.sort((a, b) => ('' + a.name.common).localeCompare(b.name.common))
 	data.forEach(pais => {
-		console.log(pais.name);
-		output += `<option>${pais.name}</option>`;
-		console.log(output);
+		console.log(pais.name.common);
+		selection += `<option>${pais.name.common}</option>`;
+		console.log(selection);
 	});
-
-selectDrop.innerHTML = output;
-
+selectDrop.innerHTML = selection;
 }).catch(error => {
 	console.error(error);
 })	
